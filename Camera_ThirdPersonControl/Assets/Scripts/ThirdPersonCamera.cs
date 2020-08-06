87165329b62552ecf9b6312d16998c509f3f51a6
KID <50666207+KID421@users.noreply.github.com>
2020-08-06 16:15:22 +0800

    攝影機

---
+++ Camera_ThirdPersonControl/Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour
+{
+    public float speed;
+    public float turn;
+    
+    private Transform target;
+    private float y;
+
+    private void Start()
+    {
+        Cursor.visible = false;
+
+        target = GameObject.Find("柴柴").transform;
+    }
+
+    private void LateUpdate()
+    {
+        Track();
+    }
+
+    private void Track()
+    {
+        Vector3 posA = target.position;
+        posA -= target.forward * 3.5f;
+        posA.y = 2f;
+
+        Vector3 posB = transform.position;
+
+        posB = Vector3.Lerp(posB, posA, Time.deltaTime * speed);
+
+        transform.position = posB;
+
+        Quaternion tar = Quaternion.LookRotation(target.position - transform.position);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, tar, Time.deltaTime * turn);
+
+        //transform.GetChild(0).Rotate(Input.GetAxis("Mouse Y") * Time.deltaTime * -turn, 0, 0);
+
+        //Vector3 eul = transform.GetChild(0).localEulerAngles;
+        //eul.x = Mathf.Clamp(transform.GetChild(0).localEulerAngles.x, -10, 10);
+        //transform.GetChild(0).localEulerAngles = eul;
+    }
+}
---
+++ Camera_ThirdPersonControl/Assets/Scripts/ThirdPersonMovement.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    public float speed;
+    public float turn;
+
+    private Rigidbody rig;
+    private Animator ani;
+
+    private void Awake()
+    {
+        rig = GetComponent<Rigidbody>();
+        ani = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        Turn();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxis("Horizontal");
+        Vector3 move = transform.forward * v + transform.right * h;
+        rig.MovePosition(transform.position + move * speed * Time.fixedDeltaTime);
+        ani.SetFloat("移動", Mathf.Abs(v) + Mathf.Abs(h));
+    }
+
+    private void Turn()
+    {
+        float x = Input.GetAxis("Mouse X");
+        transform.Rotate(transform.up * x * turn * Time.deltaTime);
+    }
+}
